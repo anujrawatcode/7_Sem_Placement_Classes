7211745d988c7f3faf411ffc5c1a6014240ea1ea
anujrawatcode <anujrawatcode@gmail.com>
2021-06-29 18:39:31 +0530

    finish

--- G3/1_Week_28June21/2_29June21/array.cpp
+++ G3/1_Week_28June21/2_29June21/array.cpp
@@ -1,9 +1,15 @@
+//Dynamic Memory allocation
+// int *p = (int*)malloc(5*sizeof(int));
+// A[i] = *(A+i)
+
+// Pointer aritmemtaic
+
 // Address of ith element of array
 // int A[3] = {5, 10, 15};
 // address of A[i] where i=2
 // Ma + i * sizeof(int) where Ma is memory address of starting element
 
-// removing duplicate from an sorted array space O(1), time O(n)
+// Q.1 removing duplicate from an sorted array space O(1), time O(n)
 // 1.Brute > 2.Better > 3.Optimal
 
 #include <bits/stdc++.h>
@@ -11,6 +17,7 @@ using namespace std;
 
 void remove_Brute(int arr[], int n)
 {
+    // Method 1: (Using extra space)
     //brute
     // compare ith element and i+1 element
     // if same do nothing
@@ -32,18 +39,43 @@ void remove_Brute(int arr[], int n)
 
     temp[k] = arr[n - 1]; //temp[k++]
     k++;
+
+    for (int i = 0; i < k; i++)
+        cout << temp[i] << " ";
 }
 
 //Home work  remove duplicate elements from a sorted array without using extra space time o(n)
 void remove_better(int arr[], int n)
 {
+    //space O(1), time O(n)
+    if (n == 0 || n == 1)
+        return;
+    int k = 0;
+    int i = 0;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        if (arr[i] != arr[i + 1])
+        {
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+
+    arr[k] = arr[n - 1]; //temp[k++]
+    k++;
+
+    for (int i = 0; i < k; i++)
+        cout << arr[i] << " ";
 }
 
 //Q2 Intersection of two sorted arrays , if duplicate , brute>better>optimal
 
 int main()
 {
-    int arr[] = {1, 1, 2, 2, 2, 3, 4, 4, 5};
     int n = 9;
-    remove_Brute(arr, n);
+    int arr[] = {1, 1, 2, 2, 2, 3, 4, 4, 5};
+    remove_better(arr, n);
+
+    //  Itersecion left
 }
